8175efcc0e7bb1cc769d08651bfd665949ffcea2
yaroslavvarkhol <yaroslav.varkhol@virtualstock.co.uk>
2026-07-05 10:45:46 +0300

    Add eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+
+export default tseslint.config(
+  { ignores: ['dist/**', '.vite/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': ['error', { ignores: ['App'] }],
+    },
+  },
+)
